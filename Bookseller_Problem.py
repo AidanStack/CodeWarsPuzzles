6a03965d535891f0893239ae693f542fd0a76059
Aidan Stack <78806930+AidanStack@users.noreply.github.com>
2021-02-25 13:50:18 -0500

    Add files via upload

---
+++ Bookseller_Problem.py
@@ -0,0 +1,34 @@
+# A bookseller has lots of books classified in 26 categories labeled A, B, ... Z. Each book has a code c of 3, 4, 5 or more characters. The 1st character of a code is a capital letter which defines the book category.
+#
+# In the bookseller's stocklist each code c is followed by a space and by a positive integer n (int n >= 0) which indicates the quantity of books of this code in stock.
+#
+# For example an extract of a stocklist could be:
+#
+# L = {"ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"}.
+# or
+# L = ["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"] or ....
+# You will be given a stocklist (e.g. : L) and a list of categories in capital letters e.g :
+#
+# M = {"A", "B", "C", "W"}
+# or
+# M = ["A", "B", "C", "W"] or ...
+# and your task is to find all the books of L with codes belonging to each category of M and to sum their quantity according to each category.
+
+def stock_list(books, cats):
+    result = ''
+    count_dict = dict.fromkeys(cats, 0)
+    for title in books:
+        count = title.split(' ')[1]
+        if title[0] in count_dict:
+            count_dict[title[0]] += int(count)
+
+    sum = 0
+    for num in count_dict.values():
+        sum += num
+    if sum == 0:
+        return ''
+
+    for element in count_dict:
+        result += '({} : {}) - '.format(element, count_dict[element])
+    result = result[:-3]
+    return result
